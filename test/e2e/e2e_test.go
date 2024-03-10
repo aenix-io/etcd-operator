d5686e3eda1cf9283656eb5dac947f908aa74b05
Hidden Marten <hiddenmarten@gmail.com>
2024-03-10 21:06:41 +0100

    Add error

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -73,8 +73,7 @@ var _ = Describe("controller", Ordered, func() {
 
 			By("installing CRDs")
 			cmd = exec.Command("make", "install")
-			// TODO: Handle CRD installation error
-			_, _ = utils.Run(cmd)
+			_, err = utils.Run(cmd)
 
 			By("deploying the controller-manager")
 			cmd = exec.Command("make", "deploy", fmt.Sprintf("IMG=%s", projectimage))
